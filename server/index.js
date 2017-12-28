6225f5a68ab76bcce5c65e70929de1ce8172c3a0
jshen18 <jennshen18@gmail.com>
2017-12-28 14:54:56 -0600

    adding get route query based on location

--- server/index.js
+++ server/index.js
@@ -16,20 +16,38 @@ app.get('/', (req, res) => {
   res.status(200).send('Inventory Service!');
 });
 
+/*
+  /experiences?neighborhood=Glen%20Park
+*/
 //  get request from client service (filter out which experiences that are available)
 app.get('/experiences', (req, res) => {
-  db.queryAsync('SELECT * FROM EXPERIENCES WHERE id > 599900')
+  const { city } = req.query;
+  const { neighborhood } = req.query;
+  
+  const neighborhoodQuery = db.queryAsync(`SELECT id FROM NEIGHBORHOOD WHERE Neighborhood='${neighborhood}' LIMIT 1`);
+  const cityQuery = db.queryAsync(`SELECT id FROM CITY WHERE City='${city}' LIMIT 1`)
+
+  Promise.all([cityQuery, neighborhoodQuery])
     .then((data) => {
-      res.status(200).json(data);
+      const [cityData, neighborhoodData] = JSON.parse(JSON.stringify(data)).map(data => data[0] || undefined);
+      const query = neighborhoodData == null ? `SELECT * FROM EXPERIENCES WHERE City_id=${cityData.id}` :
+        `SELECT * FROM EXPERIENCES WHERE City_id=${cityData.id} AND Neighborhood_id=${neighborhoodData.id}`;
+      db.queryAsync(query)
+        .then((data) => {
+          res.status(200).json(data);
+        })
+        .catch((error) => {
+          res.sendStatus(400, error);
+        });
     })
     .catch((error) => {
-      res.sendStatus(400, error);
+      console.log('ERROR', error);
     });
 });
 
 //  add reservations to table, from reservations handler
 app.post('/reservations', (req, res) => {
-  const data = Object.assign({}, req.body, { 'date_range' : JSON.stringify(req.body.date_range) });
+  const data = Object.assign({}, req.body, { 'date_range': JSON.stringify(req.body.date_range) });
   db.queryAsync('INSERT INTO EXPERIENCES SET ?', data)
     .then((data) => {
       res.json(data);
@@ -42,7 +60,7 @@ app.post('/reservations', (req, res) => {
 //  post request from vendor sending new experiences to inventory
 //  why is req body not have to filled out in postman??
 app.post('/addExperience', (req, res) => {
-  const data = Object.assign({}, req.body, { 'date_range' : JSON.stringify(req.body.date_range) });
+  const data = Object.assign({}, req.body, { 'date_range': JSON.stringify(req.body.date_range) });
   db.queryAsync('INSERT INTO experiences SET ?', data)
     .then((data) => {
       console.log('sql data inserted using post request');
